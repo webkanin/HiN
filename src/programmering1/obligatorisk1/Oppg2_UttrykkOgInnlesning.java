214b4b8520995754693915c900c019efd79a43dc
Marko Sundic <msundic@gmail.com>
2013-08-21 00:14:20 +0200

    First commit

---
+++ src/programmering1/obligatorisk1/Oppg2_UttrykkOgInnlesning.java
@@ -0,0 +1,10 @@
+package programmering1.obligatorisk1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Marko
+ * Date: 21.08.13
+ * Time: 00:05
+ */
+public class Oppg2_UttrykkOgInnlesning {
+}
---
+++ src/programmering1/obligatorisk1/Oppg5_SjekkEtTall.java
@@ -0,0 +1,10 @@
+package programmering1.obligatorisk1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Marko
+ * Date: 21.08.13
+ * Time: 00:08
+ */
+public class Oppg5_SjekkEtTall {
+}
