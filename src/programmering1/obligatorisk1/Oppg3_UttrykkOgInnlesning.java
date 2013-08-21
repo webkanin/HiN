80e2aa1ce82bae4f08f3936a95cb45f260bd0f64
Marko Sundic <msundic@gmail.com>
2013-08-21 22:38:28 +0200

    first commit obligatorisk 1

--- src/programmering1/obligatorisk1/Oppg3_UttrykkOgInnlesning.java
+++ src/programmering1/obligatorisk1/Oppg3_UttrykkOgInnlesning.java
@@ -1,4 +1,5 @@
 package programmering1.obligatorisk1;
+import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -7,4 +8,33 @@ package programmering1.obligatorisk1;
  * Time: 00:05
  */
 public class Oppg3_UttrykkOgInnlesning {
+
+    public static void main (String [] args){
+
+        double x1, y1, x2, y2; //koordinatene til to punkt
+        double distance;       //distansen mellom punktene
+
+        Scanner scan = new Scanner(System.in);
+
+        // Lese inn de to punktene
+
+        System.out.print ("Tast inn koordinatene til første punkt: " + "(bruk mellomrom mellom dem): ");
+        x1 = scan.nextDouble();
+        y1 = scan.nextDouble();
+
+        System.out.print ("Tast inn koordinatene til andre punkt: ");
+        x2 = scan.nextDouble();
+        y2 = scan.nextDouble();
+
+        //Beregningn av avstanden
+
+        distance = Math.sqrt((Math.pow((x1-x2),2))+(Math.pow((y1-y2),2)));
+
+
+        //Utskriving av svar
+        System.out.println(distance);
+
+    }
+
+
 }
