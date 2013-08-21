00bf0cf71883ba7892def972ce4d1567111bff59
Marko Sundic <msundic@gmail.com>
2013-08-21 21:27:25 +0200

    first commit obligatorisk 1

--- src/programmering1/obligatorisk1/Oppg1_Uttrykk.java
+++ src/programmering1/obligatorisk1/Oppg1_Uttrykk.java
@@ -8,5 +8,21 @@ package programmering1.obligatorisk1;
  */
 public class Oppg1_Uttrykk {
 
-    //test se om det kommer på git
+    public static void main (String[] args)
+    {
+        double tall1 = 10.0;
+        double tall2 = 2.0;
+        double tall3 = 5.0;
+
+        double gjsnitt=(tall1+tall2+tall3)/3;
+
+        System.out.println("De definerte tallverdiene er:");
+        System.out.println("Tall 1 = " + tall1);
+        System.out.println("Tall 2 = " + tall2);
+        System.out.println("Tall 3 = " + tall3);
+        System.out.println("Gjennomsnitsverdien er: " + gjsnitt);
+
+    }
+
+
 }
